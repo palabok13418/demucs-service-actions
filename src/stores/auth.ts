f2f540f90f829acbfd525ab94f4af8f733a29911
Starlight <palabokmagtiis@outlook.com>
2026-01-04 23:19:27 -0800

    Delete src directory

--- src/stores/auth.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-import type firebase from "firebase/app";
-
-const authStore = writable<firebase.UserInfo>(null);
-
-export default {
-  subscribe: authStore.subscribe,
-  set: authStore.set,
-};
